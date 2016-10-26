7a659664e4c2658d9e85e27305747c816932e312
oalaribe <obrien.alaribe@manticpoint.com>
2016-10-26 09:22:33 +0100

    Initial Commit

---
+++ Olympic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Olympic
+//
+//  Created by Obrien Alaribe on 26/10/2016.
+//  Copyright © 2016 Pam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
